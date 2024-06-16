ca02981c7c1cc683d360ebc82f0a21fbbc8e52f2
langoustinee <xmun777@naver.com>
2024-06-16 23:54:38 +0900

    feat: UserRepository 동시성 제어할 수 있도록 개선
    - 테스트 픽스처 설정을 위한 SQL 파일 추가 및 @Sql 어노테이션 선언
    - 테스트 케이스별 given절 중복 제거
    - 불필요한 DB 연결 테스트 케이스 제거

--- src/test/java/com/example/demo/repository/UserRepositoryTest.java
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -14,40 +14,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @TestPropertySource("classpath:test-application.properties")
-@Sql("/sql/user-")
+@Sql("/sql/src/test/resources/sql/user-repository-test-data.sql")
 public class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
 
     @Test
-    @DisplayName("UserRepository가 정상적으로 연결된다.")
-    void UserRepository가_정상적으로_연결된다() {
-        // given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setEmail("lango@test.com");
-        userEntity.setAddress("Seoul");
-        userEntity.setNickname("lango");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaaaa-aaa-aaaa-aaaaa");
-        // when
-        UserEntity result = userRepository.save(userEntity);
-        // then
-        assertThat(result.getId()).isNotNull();
-    }
-
-    @Test
     @DisplayName("findByIdAndStatus로 사용자 데이터를 조회할 수 있다.")
     void findByIdAndStatus로_사용자_데이터를_조회할_수_있다() {
         // given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("lango@test.com");
-        userEntity.setAddress("Seoul");
-        userEntity.setNickname("lango");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaaaa-aaa-aaaa-aaaaa");
-        userRepository.save(userEntity);
         // when
         Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
         // then
@@ -58,14 +34,6 @@ public class UserRepositoryTest {
     @DisplayName("findByIdAndStatus로 사용자 데이터를 조회할 때 데이터가 없다면 Optional empty를 반환한다.")
     void findByIdAndStatus로_사용자_데이터를_조회할_때_데이터가_없다면_Optional_empty를_반환한다() {
         // given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("lango@test.com");
-        userEntity.setAddress("Seoul");
-        userEntity.setNickname("lango");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaaaa-aaa-aaaa-aaaaa");
-        userRepository.save(userEntity);
         // when
         Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.PENDING);
         // then
@@ -76,14 +44,6 @@ public class UserRepositoryTest {
     @DisplayName("findByEmailAndStatus로 사용자 데이터를 조회할 때 데이터가 없다면 Optional empty를 반환한다.")
     void findByEmailAndStatus로_사용자_데이터를_조회할_때_데이터가_없다면_Optional_empty를_반환한다() {
         // given
-        UserEntity userEntity = new UserEntity();
-        userEntity.setId(1L);
-        userEntity.setEmail("lango@test.com");
-        userEntity.setAddress("Seoul");
-        userEntity.setNickname("lango");
-        userEntity.setStatus(UserStatus.ACTIVE);
-        userEntity.setCertificationCode("aaaaa-aaa-aaaa-aaaaa");
-        userRepository.save(userEntity);
         // when
         Optional<UserEntity> result = userRepository.findByEmailAndStatus("lango@test.com", UserStatus.PENDING);
         // then
