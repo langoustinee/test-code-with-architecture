2ae517f037731df112d3029ce952356384319542
langoustinee <xmun777@naver.com>
2024-06-18 13:01:53 +0900

    feat: 헬스체크 API 검증 테스트 코드 추가

---
+++ src/test/java/com/example/demo/controller/HealthCheckTest.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+public class HealthCheckTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("헬스 체크 API 엔드포인트 호출시 응답 상태코드는 200을 반환한다.")
+    void 헬스_체크_API_엔드포인트_호출시_응답_상태코드는_200을_반환한다() throws Exception {
+        // given
+        // when
+        // then
+        mockMvc.perform(MockMvcRequestBuilders.get("/health_check.html"))
+                .andExpect(status().isOk());
+    }
+
+}
