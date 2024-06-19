dce50dd2141e1b57fb6d84941112b5080f7273e6
langoustinee <xmun777@naver.com>
2024-06-19 13:00:11 +0900

    feat: User(사용자) 생성 컨트롤러 테스트 코드 추가

---
+++ src/test/java/com/example/demo/controller/UserCreateControllerTest.java
@@ -0,0 +1,66 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.dto.UserCreateDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+@SqlGroup({
+        @Sql(value = "/sql/delete-all-data.sql", executionPhase = AFTER_TEST_METHOD)
+})
+public class UserCreateControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private JavaMailSender javaMailSender;
+
+    private final ObjectMapper om = new ObjectMapper();
+
+    @Test
+    @DisplayName("회원가입 API를 호출하면 새로운 사용자를 등록할 수 있고 회원가입된 사용자는 PENDING 상태가 된다.")
+    void 회원가입_API를_호출하면_새로운_사용자를_등록할_수_있고_회원가입된_사용자는_PENDING_상태가_된다() throws Exception {
+        // given
+        UserCreateDto request = UserCreateDto.builder()
+                .email("lango@test.com")
+                .nickname("lango")
+                .address("Pangyo")
+                .build();
+        BDDMockito.doNothing().when(javaMailSender).send(any(SimpleMailMessage.class));
+        // when
+        // then
+        mockMvc.perform(post("/api/users")
+                        .header("EMAIL", "lango@test.com")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(om.writeValueAsString(request))
+                )
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.email").value("lango@test.com"))
+                .andExpect(jsonPath("$.nickname").value("lango"))
+                .andExpect(jsonPath("$.status").value("PENDING"));
+    }
+
+}
