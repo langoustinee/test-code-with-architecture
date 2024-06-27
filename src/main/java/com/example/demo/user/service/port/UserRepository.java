12ceb26b25388b0be9ede45180b6e876e5ecec65
langoustinee <xmun777@naver.com>
2024-06-28 01:19:06 +0900

    refactor: PostService가 가지는 UserService 의존성 제거
    - UserService가 아닌 UserRepository 인터페이스를 의존하도록 변경

--- src/main/java/com/example/demo/user/service/port/UserRepository.java
+++ src/main/java/com/example/demo/user/service/port/UserRepository.java
@@ -2,11 +2,13 @@ package com.example.demo.user.service.port;
 
 import com.example.demo.user.domain.User;
 import com.example.demo.user.domain.UserStatus;
-import com.example.demo.user.infrastructure.UserEntity;
 
 import java.util.Optional;
 
 public interface UserRepository {
+
+
+    User getById(Long id);
 
     Optional<User> findByIdAndStatus(long id, UserStatus userStatus);
 
