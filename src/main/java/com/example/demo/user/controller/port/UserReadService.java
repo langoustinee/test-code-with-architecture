690f4f596a97b5b7cbbbe27dca60ce11a94386c9
langoustinee <xmun777@naver.com>
2024-06-30 21:41:13 +0900

    feat: 컨트롤러 소형 테스트 구축을 위한 서비스 인터페이스 분리
    - UserService 인터페이스 메서드 책임을 그룹화한 개별 인터페이스로 분리

--- src/main/java/com/example/demo/user/controller/port/UserReadService.java
+++ src/main/java/com/example/demo/user/controller/port/UserReadService.java
@@ -1,8 +1,6 @@
 package com.example.demo.user.controller.port;
 
 import com.example.demo.user.domain.User;
-import com.example.demo.user.domain.UserCreate;
-import com.example.demo.user.domain.UserUpdate;
 
 public interface UserReadService {
 
